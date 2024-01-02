4632e3cf39cc5f73015c809f1258369abfdabdee
smasram1609 <smasram1609@gmail.com>
2024-01-03 01:35:24 +0530

    [S] Update Myntra tc added

--- src/test/java/e_commerce/myntra.java
+++ src/test/java/e_commerce/myntra.java
@@ -1,9 +1,22 @@
 package e_commerce;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class myntra {
   @Test
-  public void f() {
+  public void myntraTest() {
+	  
+	  Reporter.log("Opening Myntra Page", true);
+	  System.setProperty("webdriver.chrome.driver", "D:\\Chrome Driver\\chromedriver-win64\\chromedriver.exe");
+	  ChromeOptions opt = new ChromeOptions();
+	  opt.addArguments("--remote-allow-origins=*");
+	  WebDriver driver = new ChromeDriver(opt);
+	  driver.get("https://www.myntra.com");
+	  driver.manage().window().maximize();
+	  driver.quit();
   }
 }
